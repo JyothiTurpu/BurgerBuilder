598a5dd79fe3a3fe1612f3e4c4251f2efff547cf
Jyothi Turpu <jyothi.turpu@gmail.com>
2020-09-24 04:15:14 -0400

    Completed Basic App

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,31 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux/Aux";
 
 import classes from "./SideDrawer.css";
 
 const SideDrawer = (props) => {
-  return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
-      </div>
+  let attachedClasses = [classes.SideDrawer, classes.Close];
 
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
   );
 };
 
